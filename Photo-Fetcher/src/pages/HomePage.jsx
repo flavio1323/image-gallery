7fb6317a158c438c23eb8329e42cde8af7b958c7
flavio1323 <flaviobregu1@gmail.com>
2025-03-15 16:02:27 -0500

    Add store folder and implement dummyData for dynamic content

--- Photo-Fetcher/src/pages/HomePage.jsx
+++ Photo-Fetcher/src/pages/HomePage.jsx
@@ -2,20 +2,14 @@ import React from "react";
 import Controls from "../components/UI/Controls";
 import ImageContainer from "../components/UI/ImageContainer";
 import MainLayout from "../layouts/MainLayout";
+import dummyData from "../store/dummyData";
 
 const HomePage = () => {
-    const images = [
-        { id: 1, src: "https://picsum.photos/200", alt: "Photo 1" },
-        { id: 2, src: "https://picsum.photos/201", alt: "Photo 2" },
-        { id: 3, src: "https://picsum.photos/202", alt: "Photo 3" },
-        { id: 4, src: "https://picsum.photos/203", alt: "Photo 4" },
-    ];
-
     return (
         <MainLayout>
             <Controls />
             <div className="image-grid">
-                {images.map((image) => (
+                {dummyData.map((image) => (
                     <ImageContainer key={image.id} src={image.src} alt={image.alt} />
                 ))}
             </div>
