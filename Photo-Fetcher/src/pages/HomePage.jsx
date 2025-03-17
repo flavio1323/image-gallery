f3e1bc2d1e46914a9e97dfcac0ff1ee511fba4fa
flavio1323 <flaviobregu1@gmail.com>
2025-03-17 00:40:20 -0500

    Add services folder and implement API integration

--- Photo-Fetcher/src/pages/HomePage.jsx
+++ Photo-Fetcher/src/pages/HomePage.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Controls from "../components/UI/Controls";
 import ImageContainer from "../components/UI/ImageContainer";
 import MainLayout from "../layouts/MainLayout";
-import dummyData from "../store/dummyData";
+import { fetchPhotos } from "../services/api";
 
 const HomePage = () => {
+    const [photos, setPhotos] = useState([]);
+
+    useEffect(() => {
+        const getPhotos = async () => {
+            const fetchedPhotos = await fetchPhotos();
+            setPhotos(fetchedPhotos);
+        };
+
+        getPhotos();
+    }, []);
+
     return (
         <MainLayout>
             <Controls />
             <div className="image-grid">
-                {dummyData.map((image) => (
+                {photos.map((image) => (
                     <ImageContainer key={image.id} src={image.src} alt={image.alt} />
                 ))}
             </div>
